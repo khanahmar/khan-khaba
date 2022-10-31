0c7b077167f4ddd14d675e4a2c8b258a6f5ce247
Ahmar khan <ahmarbilal223344@gmail.com>
2022-10-31 21:45:05 +0500

    changes done

--- admin/admin.js
+++ admin/admin.js
@@ -7,6 +7,8 @@ import {
   collection,
   getDocs,
   addDoc,
+  doc,
+  deleteDoc,
 } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -27,7 +29,7 @@ const db = getFirestore(app);
 
 // Read  Data
 const querySnapshot = await getDocs(collection(db, "menu"));
-console.log(querySnapshot);
+
 let i = 1;
 querySnapshot.forEach((doc) => {
   display.innerHTML += `  <tr>
@@ -36,6 +38,7 @@ querySnapshot.forEach((doc) => {
   <td>${doc.data().title}</td>
   <td>${doc.data().desc}</td>
   <td>${doc.data().price}</td>
+  <td><button  class="btn btn-danger">Delete</button></td>
 </tr>
 `;
 });
@@ -55,7 +58,7 @@ form.addEventListener("submit", async (e) => {
     desc,
     price,
   };
-  console.log(pizza);
+
   try {
     const docRef = await addDoc(collection(db, "menu"), pizza);
     console.log("Document written with ID: ", docRef.id);
@@ -63,3 +66,8 @@ form.addEventListener("submit", async (e) => {
     console.error("Error adding document: ", e);
   }
 });
+
+const deleteItem = async (id) => {
+  let itmDelt = await deleteDoc(doc(db, "menu", id));
+  console.log(itmDelt);
+};
